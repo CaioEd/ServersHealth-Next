b1aff73c51bd3b11cbb92bef62d7cc3da1599a2a
CaioEd <caioersb@gmail.com>
2026-01-22 21:31:39 -0300

    select content btn

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,18 @@
+"use client";
+
 import DashboardCards from "@/components/dashboard/DashboardCards";
+import DataSelects from "@/components/dashboard/DataSelects";
 
 export default function Home() {
+
   return (
     <div className="w-full min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <div className="flex flex-col justify-center p-4">
         <h1 className="text-2xl font-bold">Informações gerais</h1>
         <p className="text-zinc-500">Aqui você pode ver as informações gerais sobre os seus servidores</p>
         <DashboardCards />
+
+        <DataSelects />
       </div>
     </div>
   );
